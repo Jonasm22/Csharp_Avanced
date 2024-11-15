16d43e507ea35d2232e5a4fe4680c46483404964
Jonathan Mendoza <john.2114@hotmail.com>
2024-11-16 00:44:33 +0100

    Update

---
+++ DeconstructCustom/Planet.cs
@@ -0,0 +1,43 @@
+// Example file for LinkedIn Learning Course "Advanced C#: Language Features by Joe Marini"
+// Using Deconstruction on a custom defined type
+
+public class Planet
+{
+    public string Name { get; set; }
+    public int Radius { get; set; }
+    public int MoonCount { get; set; }
+    public int DistanceFromSunKm { get; set; }
+
+    public Planet(string name, int rads, int moons, int distance ) {
+        Name = name;
+        Radius = rads;
+        MoonCount = moons;
+        DistanceFromSunKm = distance;
+    }
+
+    // TODO: Define a Deconstruct method to return the name and moon count
+
+    public void Deconstruct(out string name, out int moons)
+    {
+        name = Name;
+        moons = MoonCount;
+    }
+
+    // TODO: Define a Deconstruct method to return the name, moon count, and radius
+
+    public void Deconstruct(out string name, out int moons, out int radius)
+    {
+        name = Name;
+        moons = MoonCount;
+        radius = Radius ;
+    }
+
+    // TODO: Defining a Deconstruct with the name number of parameters will cause an error
+
+    public void Descontruct(out string name, out int distance)
+    {
+        name = Name;
+        distance = DistanceFromSunKm;
+        
+    }
+}
---
+++ DeconstructCustom/Program.cs
@@ -0,0 +1,16 @@
+﻿// Example file for LinkedIn Learning Course "Advanced C#: Language Features by Joe Marini"
+// Using Deconstruction on a custom defined type
+
+// Create some classes with data
+Planet Earth = new Planet("Earth", 6371, 1, 150_980_000);
+Planet Venus = new Planet("Venus", 6052, 0, 108_930_000);
+
+// TODO: Use Deconstruction to get the name and moon count
+
+var (name, moons) = Earth;
+Console.WriteLine($"{name}, {moons}");
+
+// TODO: Use Deconstruction to get the name, moon count, and radius
+
+(name, moons, var radius) = Venus;
+Console.WriteLine($"{name}, {moons}, {radius}");
